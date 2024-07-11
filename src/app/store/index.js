6c20eef98e9d3876b24c75eb6ddba492d3db155b
stetson harding <stetson2001@msn.com>
2024-07-11 09:08:24 -0700

    Added new task with redux and saga

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,8 @@
 //provides state to application
 
-import {createStore, applyMiddleware, combineReducers} from 'redux'
-import {defaultState} from "../../server/defaultState"
-import {createLogger} from 'redux-logger'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { defaultState } from "../../server/defaultState"
+import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import * as sagas from './sagas.mock'
 import * as mutations from './mutations'
@@ -10,25 +10,32 @@ import * as mutations from './mutations'
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
-  
-       combineReducers({
-        tasks(tasks= defaultState.tasks, action) {
-            switch(action.type) {
+
+    combineReducers({
+        tasks(tasks = defaultState.tasks, action) {
+            switch (action.type) {
                 case mutations.CREATE_TASK:
-                console.log(action)
+                    return [...tasks, {
+                        id:action.taskID,
+                       name: 'New Task',
+                       group: action.groupID,
+                       owner: action.ownerID,
+                       isComplete: false
+
+                    } ]
             }
             return tasks;
         },
-        comments(comments= defaultState.comments) {
+        comments(comments = defaultState.comments) {
             return comments
         },
-        groups(groups= defaultState.groups) {
+        groups(groups = defaultState.groups) {
             return groups
         },
-        users(users=defaultState.users) {
+        users(users = defaultState.users) {
             return users
         }
-       }),
+    }),
 
     applyMiddleware(createLogger(), sagaMiddleware)
 )
