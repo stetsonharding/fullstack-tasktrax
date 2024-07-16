32af1b29708372b9895aeaa944b062b309ff267b
stetson harding <stetson2001@msn.com>
2024-07-16 10:05:07 -0700

    server function to delete a task

--- src/server/server.spec.js
+++ src/server/server.spec.js
@@ -1,15 +1,19 @@
-import {addNewTask, updateTask} from './server'
+import {addNewTask, updateTask, deleteTask} from './server'
 
 
 (async function myFuncTest () {
-     await addNewTask({
-        name: 'My Task Number 2',
-        id:'12345'
-    })
+    //  await addNewTask({
+    //     name: 'My Task Number 2',
+    //     id:'12345'
+    // })
 
-    await updateTask({
-        id:'1234',
-        name: 'task1234 -- UPDATED!'
+    // await updateTask({
+    //     id:'1234',
+    //     name: 'task1234 -- UPDATED!'
+    // })
+
+    await deleteTask({
+        id:'12345',
     })
 
 })();
