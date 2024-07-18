dc61bba2bde4f3f962e6c21610f694ee9fb9ee3e
stetson harding <stetson2001@msn.com>
2024-07-18 09:58:01 -0700

    Added route gaurd and inital login page

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -6,6 +6,19 @@ import { Route, Router } from "react-router"
 import { history } from '../store/History';
 import { ConnectedNavigation } from './Navigation'
 import { ConnectedTaskDetail } from '../components/TaskDetails'
+import { ConnectedLogin } from '../components/Login';
+import { Redirect } from 'react-router';
+
+
+const RouteGaurd = Component => ({match}) => {
+   //Logic to check what component should be returned
+   console.info("Route Gaurd", match)
+   if(!store.getState().session.authenticated){
+    return <Redirect to="/"/>
+   }
+   return <Component match={match} />
+}
+
 
 export const App = () => (
    <Router history={history}>
@@ -14,8 +27,9 @@ export const App = () => (
          <div>
             <ConnectedNavigation />
 
-            <Route exact path="/Dashboard" render={() => <ConnectedDashboard />} />
-            <Route exact path='/taskDetail/:id' render={({ match }) => (<ConnectedTaskDetail match={match} />)} />
+            <Route exact path='/' component={ConnectedLogin} />
+            <Route exact path="/Dashboard" render={RouteGaurd(ConnectedDashboard)} />
+            <Route exact path='/taskDetail/:id' render={RouteGaurd(ConnectedTaskDetail)} />
          </div>
 
       </Provider>
