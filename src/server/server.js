0319d4558bd39cc50872c73ba1db69e8a8e918c5
stetson <stetson2001@msn.com>
2024-07-25 22:26:44 -0700

    fix for cors origin

--- src/server/server.js
+++ src/server/server.js
@@ -17,9 +17,15 @@ let port = process.env.PORT || 7777;
 let app = express();
 app.listen(port, console.log('Server lisening on port', port))
 
+// Allow requests from your frontend origin
+const corsOptions = {
+    origin: 'https://tasktrax-691ef54c2e05.herokuapp.com',
+    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 //Plugins used in application
 app.use(
-    cors(),
+    cors(corsOptions),
     //Let us use post requests
     bodyParser.urlencoded({ extended: true }),
     bodyParser.json()
