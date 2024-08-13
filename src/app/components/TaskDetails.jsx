22cf49cbc80c9abc1f57237a4c1ed57e02196a4b
stetson harding <stetson2001@msn.com>
2024-08-13 10:36:27 -0700

    Inital edit comment

--- src/app/components/TaskDetails.jsx
+++ src/app/components/TaskDetails.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import * as mutations from "../store/mutations";
@@ -16,7 +16,12 @@ export const TaskDetails = ({
   isComplete,
   setTaskComplete,
   sessionID,
+  comment
 }) => {
+
+  const [commentContent, setCommentContent] = useState(comment.content || "")
+
+  console.log(comment)
   return (
     <>
       <div>
@@ -46,7 +51,7 @@ export const TaskDetails = ({
       {/* Comments form */}
       <form
         className="form-inline"
-        onSubmit={(e) => addTaskComment(id, sessionID, e)}
+        onSubmit={(e) => addTaskComment(id, sessionID, e, commentContent)}
       >
         <input
           className="form-control"
@@ -54,6 +59,8 @@ export const TaskDetails = ({
           name="commentContent"
           autoComplete="off"
           placeholder="Add a Comment"
+        value={commentContent}
+        onChange={(e) => setCommentContent(e.target.value)}
         />
 
         <button type="submit">Done</button>
@@ -72,10 +79,12 @@ const mapStateToProps = (state, ownProps) => {
   let id = ownProps.match.params.id;
   let task = state.tasks.find((task) => task.id === id);
   let isOwner = (state.session.id = task.owner);
+  let comment = state.comments.find(comment => comment.task === id)
   let groups = state.groups;
 
   return {
     id,
+    comment,
     task,
     isOwner,
     sessionID: state.session.id,
@@ -99,14 +108,14 @@ const mapDispatchToProps = (dispatch, ownProps) => {
     setDeleteTask(id) {
       dispatch(mutations.setDeleteTask(id));
     },
-    addTaskComment(taskID, ownerID, e) {
+    addTaskComment(taskID, ownerID, e, content) {
       e.preventDefault();
       console.log("ran");
-      let input = e.target["commentContent"];
+      // let input = e.target["commentContent"];
       let commentID = uuid();
-      let content = input.value;
+      // let content = input.value;
       if (content !== "") {
-        input.value = "";
+      
         dispatch(mutations.addTaskComment(commentID, taskID, ownerID, content));
       }
     },
