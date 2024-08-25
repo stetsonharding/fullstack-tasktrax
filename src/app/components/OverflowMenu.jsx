51ea71a94e4d934ff32979c3af658d676d1dcd3e
stetson <stetson2001@msn.com>
2024-08-25 07:35:11 -0700

    Edit and delete button functionality for overflowMenu

--- src/app/components/OverflowMenu.jsx
+++ src/app/components/OverflowMenu.jsx
@@ -3,37 +3,33 @@ import { connect } from "react-redux";
 import * as mutations from "../store/mutations";
 import { Link } from "react-router-dom";
 
+const OverflowMenu = ({ id, setDeleteTask }) => {
+  return (
+    <div className="overflow-menu mt-1 d-flex flex-column justify-content-end align-items-end w-full">
+      <Link to={`/taskDetail/${id}`} className="w-25">
+        <button type="button" className="btn btn-light btn-sm w-100">Edit</button>
+      </Link>
 
-const OverflowMenu = ({id, setDeleteTask}) => {
-
-    return (
-        <div className="overflow-menu mt-1 d-flex flex-column justify-content-end align-items-end w-full">
-           <Link  to={`/taskDetail/${id}`}>
-            <button className="btn btn-light mb-1 btn-sm w-25" >Edit</button>
-           </Link>
-            <button className="btn btn-light  btn-sm w-25" onClick={() => setDeleteTask(id)}>Delete</button>
-        </div>
-    );
+      <button 
+        onClick={() => setDeleteTask(id)} 
+        type="button" 
+        className="btn btn-light w-25 mt-1 btn-sm">
+        Delete
+      </button>
+    </div>
+  );
 };
 
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-
-    return {
-     
-     
-     
-      setDeleteTask(id) {
-        dispatch(mutations.setDeleteTask(id));
-      },
-     
-    };
+  return {
+    setDeleteTask(id) {
+      dispatch(mutations.setDeleteTask(id));
+    },
   };
-  
-  export const ConnectedOverflowMenu = connect(
-null,
-    mapDispatchToProps
-  )(OverflowMenu);
-  
+};
 
-
+export const ConnectedOverflowMenu = connect(
+  null,
+  mapDispatchToProps
+)(OverflowMenu);
