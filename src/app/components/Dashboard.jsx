03b1f2b821190ef258d035dd6f35ddb516f22c1e
stetson harding <stetson2001@msn.com>
2024-08-27 09:23:31 -0700

    task overview lengths for each group

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -5,14 +5,16 @@ import { ConnectedTaskList } from './TaskList'
 import { ConnectedNavigation } from './Navigation';
 import TaskOverview from './TaskOverview';
 
-export const Dashboard = ({ groups }) => {
+export const Dashboard = ({ groups, toDoLength, doingLength, doneLength }) => {
+
+   
     return (
-        <div className="container-fluid d-flex m-2 flex-column justify-content-start" style={{height: '100%'}}>
+        <div className="container-fluid d-flex m-1 flex-column justify-content-start" style={{height: '100%'}}>
             <ConnectedNavigation />
-            <div className="d-flex justify-content-around align-items-start w-100"> 
-                <TaskOverview />
-                <TaskOverview />
-                <TaskOverview />
+            <div className="row d-flex justify-content-around align-items-start w-100">
+                <TaskOverview name="To Do's" length={toDoLength} />
+                <TaskOverview name="Doing" length={doingLength} />
+                <TaskOverview name="Done" length={doneLength}/> 
             </div>
             <div className="row w-100 h-100">
             {groups.map(group => (
@@ -24,8 +26,15 @@ export const Dashboard = ({ groups }) => {
 };
 
 const mapStateToProps = (state) => {
+    let toDoLength = state.tasks.filter(task => task.group === 'G1').length;
+    let doingLength = state.tasks.filter(task => task.group === 'G2').length;
+    let doneLength = state.tasks.filter(task => task.group === 'G3').length;
+
     return {
-        groups: state.groups
+        groups: state.groups,
+        toDoLength,
+        doneLength,
+        doingLength
     }
 
 }
--- src/app/components/TaskOverview.jsx
+++ src/app/components/TaskOverview.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function TaskOverview() {
+function TaskOverview(props) {
   return (
     <div className="card mt-4" style={{width: '18rem', height: 'fit-content'}}>
   <div className="card-body">
-    <h5 className="card-title">Todo's</h5>
-    <p className="card-text">15 task to dos</p>
+    <h5 className="card-title">{props.name}</h5>
+    <p className="card-text">{props.length}</p>
   </div>
 </div>
   )
